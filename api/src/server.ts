763f98c7fff36438a7b4e903fe343385454ba2c6
Lucas Cascão <lucas_cascao_@outlook.com>
2021-03-01 09:05:27 -0300

    Inserido teste com jest

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,19 +1,3 @@
-import 'reflect-metadata';
-import express, { request, response } from 'express';
-import "./database"
-import { router } from './routes';
-
-/**
- * GET => Busca
- * POST => Salvar
- * PUT => Alterar
- * DELETE => Deletar
- * PATCH => Alteração especifica
- */
-
-const app = express();
-
-app.use(express.json())
-app.use(router)
+import { app } from './app';
 
 app.listen(4500, () =>  console.log("Server is running!"));
